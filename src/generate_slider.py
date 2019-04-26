c56d6bc9eca891b38b874a3b20d5fd15bd7ee3fa
shintaroDesu <snkraishin87@gmail.com>
2019-04-26 18:51:56 +0200

    clean up code and put code in class Generate Slider

--- src/generate_slider.py
+++ src/generate_slider.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from PIL import Image
 import cv2
@@ -6,112 +5,100 @@ import aggdraw
 from curve import *
 
 
-def convert(string):
-	string = string.split(",")
-	ps = [Position(int(string[0]), int(string[1]))]
-	slider_path = string[5]
-	slider_path = slider_path.split("|")
-	slider_path = slider_path[1:]
+class GenerateSlider:
+	def __init__(self, sliderborder, slideroverride, radius):
+		self.sliderborder = sliderborder
+		self.sliderborder[0], self.sliderborder[2] = self.sliderborder[2], self.sliderborder[0]
+		self.sliderborder = tuple(self.sliderborder)
 
-	for pos in slider_path:
-		pos = pos.split(":")
-		ps.append(Position(int(pos[0]), int(pos[1])))
+		self.slideroverride = slideroverride
+		self.slideroverride[0], self.slideroverride[2] = self.slideroverride[2], self.slideroverride[0]
+		self.slideroverride = tuple(self.slideroverride)
 
-		end_point = [int(pos[0]), int(pos[1])]
+		self.radius = radius
 
-	pixel_length = float(string[7])
-	return ps, pixel_length#, end_point
+	def convert(self, slider_code):
+		string = slider_code.split(",")
+		ps = [Position(int(string[0]), int(string[1]))]
+		slider_path = string[5]
+		slider_path = slider_path.split("|")
+		slider_path = slider_path[1:]
+
+		for pos in slider_path:
+			pos = pos.split(":")
+			ps.append(Position(int(pos[0]), int(pos[1])))
+
+		pixel_length = float(string[7])
+		return ps, pixel_length
+
+	def get_pos_from_bezier(self, baiser_class):
+		cur_pos = baiser_class(0)
+		t = 0.02
+		curve_pos = [int(cur_pos.x), int(cur_pos.y)]
+		while t <= 1:
+			cur_pos = baiser_class.at(t)
+			curve_pos.append(int(cur_pos[0][0]))
+			curve_pos.append(int(cur_pos[0][1]))
+			t += 0.02
+		return curve_pos
+
+	def get_min_max(self, curve_pos):
+		min_x, min_y, max_x, max_y = curve_pos[0], curve_pos[1], curve_pos[0], curve_pos[1]
+		for index in range(0, len(curve_pos), 2):
+			min_x = min(min_x, curve_pos[index])
+			min_y = min(min_y, curve_pos[index + 1])
+
+			max_x = max(max_x, curve_pos[index])
+			max_y = max(max_y, curve_pos[index + 1])
+
+		return min_x, min_y, max_x, max_y
+
+	def get_pos(self, x1, y1, radius):
+		x_border1 = x1 - radius
+		y_border1 = y1 - radius
+		x_border2 = x1 + radius
+		y_border2 = y1 + radius
+
+		return x_border1, y_border1, x_border2, y_border2
+
+	def draw(self, curve_pos):
+		to_color = np.array([50, 50, 50])
+		im = Image.new('RGBA', (512, 384), (0, 0, 0, 0))
+		draw = aggdraw.Draw(im)
+		draw.setantialias(True)
+
+		xstart1, ystart1, xstart2, ystart2 = self.get_pos(curve_pos[0], curve_pos[1], self.radius)
+		xend1, yend1, xend2, yend2 = self.get_pos(curve_pos[-2], curve_pos[-1], self.radius)
+		draw.ellipse((xstart1, ystart1, xstart2, ystart2), None, aggdraw.Brush(self.sliderborder))
+		draw.ellipse((xend1, yend1, xend2, yend2), None, aggdraw.Brush(self.sliderborder))
+
+		draw.line(curve_pos, aggdraw.Pen(self.sliderborder, int(self.radius) * 2))
+		draw.line(curve_pos, aggdraw.Pen(self.slideroverride, int(self.radius - 8) * 2))
+		for c in range(4, int(self.radius), 2):
+			coefficient = c / self.radius
+			cur_slider = to_color * coefficient + np.array(self.slideroverride)
+			cur_slider[cur_slider > 255] = 255
+			cur_slider = cur_slider.astype(int)
+			cur_slider = tuple(cur_slider)
+			draw.ellipse((xstart1 + c, ystart1 + c, xstart2 - c, ystart2 - c), None, aggdraw.Brush(cur_slider))
+			draw.ellipse((xend1 + c, yend1 + c, xend2 - c, yend2 - c), None, aggdraw.Brush(cur_slider))
+			draw.line(curve_pos, aggdraw.Pen(cur_slider, int(self.radius * 2 - c * 2)))
+		draw.flush()
+		return im
+
+	def get_slider_img(self, slider_code):
+		ps, pixel_length = self.convert(slider_code)
+		baiser = Bezier(ps, pixel_length)
+		curve_pos = self.get_pos_from_bezier(baiser)
+		min_x, min_y, max_x, max_y = self.get_min_max(curve_pos)
+		im = self.draw(curve_pos)
+		img = np.array(im)
+		img = img[int(min_y - self.radius):math.ceil(max_y + self.radius),
+		      int(min_x - self.radius):math.ceil(max_x + self.radius)]
+		return img
 
 
-def get_pos_from_bezier(test):
-	cur_pos = test(0)
-	t = 0.02
-	curve_pos = [int(cur_pos.x), int(cur_pos.y)]
-	while t <= 1:
-		cur_pos = test.at(t)
-		curve_pos.append(int(cur_pos[0][0]))
-		curve_pos.append(int(cur_pos[0][1]))
-		t += 0.02
-
-	return curve_pos
-
-
-def rotate_image(mat, angle):
-	"""
-	Rotates an image (angle in degrees) and expands image to avoid cropping
-	"""
-
-	height, width = mat.shape[:2] # image shape has 3 dimensions
-	image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
-
-	rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
-
-	# rotation calculates the cos and sin, taking absolutes of those.
-	abs_cos = abs(rotation_mat[0,0])
-	abs_sin = abs(rotation_mat[0,1])
-
-	# find the new width and height bounds
-	bound_w = int(height * abs_sin + width * abs_cos)
-	bound_h = int(height * abs_cos + width * abs_sin)
-
-	# subtract old image center (bringing image back to origo) and adding the new image center coordinates
-	rotation_mat[0, 2] += bound_w/2 - image_center[0]
-	rotation_mat[1, 2] += bound_h/2 - image_center[1]
-
-	# rotate image with the new bounds and translated rotation matrix
-	rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
-	return rotated_mat
-
-
-def get_pos(x1, y1, radius):
-	x_border1 = x1 - radius
-	y_border1 = y1 - radius
-	x_border2 = x1 + radius
-	y_border2 = y1 + radius
-
-	return x_border1, y_border1, x_border2, y_border2
-
-
-def draw(sliderborder, slideroverride, radius, curve_pos):
-	to_color = np.array([50, 50, 50])
-	im = Image.new('RGBA', (512, 384), (0, 0, 0, 0))
-	draw = aggdraw.Draw(im)
-	draw.setantialias(True)
-
-	xstart1, ystart1, xstart2, ystart2 = get_pos(curve_pos[0], curve_pos[1], radius)
-	xend1, yend1, xend2, yend2 = get_pos(curve_pos[-2], curve_pos[-1], radius)
-	draw.ellipse((xstart1, ystart1, xstart2, ystart2), None, aggdraw.Brush(sliderborder))
-	draw.ellipse((xend1, yend1, xend2, yend2), None, aggdraw.Brush(sliderborder))
-
-	draw.line(curve_pos, aggdraw.Pen(sliderborder, int(radius) * 2))
-	draw.line(curve_pos, aggdraw.Pen(slideroverride, int(radius - 8) * 2))
-	for c in range(4, int(radius), 2):
-		coefficient = c/radius
-		cur_slider = to_color * coefficient + np.array(slideroverride)
-		cur_slider[cur_slider > 255] = 255
-		cur_slider = cur_slider.astype(int)
-		cur_slider = tuple(cur_slider)
-		draw.ellipse((xstart1 + c, ystart1 + c, xstart2 - c, ystart2 - c), None, aggdraw.Brush(cur_slider))
-		draw.ellipse((xend1 + c, yend1 + c, xend2 - c, yend2 - c), None, aggdraw.Brush(cur_slider))
-		draw.line(curve_pos, aggdraw.Pen(cur_slider, int(radius*2 - c*2)))
-	draw.flush()
-	return im
-
-
-# for x in range(1000):
-code = "150,131,0,2,0,B|362:78|338:306|172:295,1,375"
-ps, pixel_length = convert(code)
-test = Bezier(ps, pixel_length)
-curve_pos = get_pos_from_bezier(test)
-
-color = [255, 69, 0]
-color[0], color[2] = color[2], color[0]
-color = tuple(color)
-override = [0, 60, 120]
-override[0], override[2] = override[2], override[0]
-override = tuple(override)
-
-circle = cv2.imread("dessous.png")
-im = draw(color, override, 36.48, curve_pos)
-img = np.array(im)
-cv2.imwrite("test.png", img)
+if __name__ == "__main__":
+	slidercode = "150,131,0,2,0,B|362:78|338:306|172:295,1,375"
+	gs = GenerateSlider([255, 69, 0], [0, 60, 120], 36.48)
+	cv2.imwrite("test.png", gs.get_slider_img(slidercode))
