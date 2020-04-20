4f269f28cafbe7f9579f803d65b876a97ce99b62
shintaroDesu <snkraishin87@gmail.com>
2020-04-20 18:53:40 +0200

    index out of range sliderball

--- src/Objects/abstracts.py
+++ src/Objects/abstracts.py
@@ -176,8 +176,8 @@ class AnimatableImage:
 			should_continue = os.path.isfile(self.path + self.filename + self.delimiter + str(counter) + ".png")
 		if not self.frames:
 			self.frames.append(Images(self.path + self.filename, self.scale, rotate))
-		else:
-			self.frames.pop(0)
+		# else:
+		# 	self.frames.pop(0)
 
 	def prepare_frames(self):
 		pass
