8648e6ac1964d88f9cb4ed2717377522610573df
shintaroDesu <snkraishin87@gmail.com>
2020-04-24 01:56:51 +0200

    optimise urbar by avoiding drawing multiple bars everytime

--- src/Objects/Scores/URBar.py
+++ src/Objects/Scores/URBar.py
@@ -10,7 +10,7 @@ class URBar(Images):
 		self.x_offset = self.x - self.w // 2
 		self.barheight = int(self.h/5)
 
-		self.colors = [(255, 210, 50, 150), (80, 255, 50, 150), (60, 205, 255, 150)]
+		self.colors = [(255, 200, 77, 175), (89, 255, 9, 175), (44, 186, 255, 175)]
 
 		self.maxtime = scorewindow[2]
 		self.widths = [int(self.w),
@@ -21,39 +21,34 @@ class URBar(Images):
 		self.resultdict = {50: 0, 100: 1, 300: 2}
 
 		self.bar_images = []
+		self.timer = 0
 		self.prepare_bar()
 
 	def prepare_bar(self):
 		for i in range(3):
 			self.bar_images.append(Image.new("RGBA", (4, self.h), self.colors[i]))
+		self.bar_container = Image.new("RGBA", (self.w, self.h))
 
 		self.urbar = Image.new("RGBA", (self.w, self.h))
 		for i in range(len(self.xstart)):
 			self.urbar.paste(self.colors[i], (self.xstart[i], self.barheight * 2, self.xstart[i] + self.widths[i], self.barheight * 3))
 
 	def add_bar(self, delta_t, hitresult):
+		asdf = time.time()
 		pos = int(self.w/2 + delta_t/self.maxtime * self.w/2)
-		self.bars.append([pos, 200, self.resultdict[hitresult]])
+		# self.bars.append([pos, 1, self.resultdict[hitresult]])
+		self.img = self.bar_images[self.resultdict[hitresult]]
+		super().add_to_frame(self.bar_container, pos, self.bar_container.size[1]//2)
+		s = self.bar_container.size
+		self.bar_container.paste((255, 255, 255, 255), (s[0]//2 - 1, 0, s[0]//2 + 1, s[1]))
+		self.timer += time.time() - asdf
 
-	def add_to_frame_bar(self, background):
-		self.img = self.urbar
-		super().add_to_frame(background, self.x, self.y)
-		self.img = Image.new("RGBA", (4, self.h), (255, 255, 255, 255))
-		super().add_to_frame(background, self.x, self.y)
-
-	def determine(self, i):
-		background = self.bg
-		bar = self.bars[i]
-		self.img = self.bar_images[bar[2]]
-		super().add_to_frame(background, self.x_offset + bar[0], self.y, alpha=bar[1]/200)
-		bar[1] -= 1
-		if bar[1] <= 0:
-			return False
-		return True
 
 	def add_to_frame(self, background):
 		self.bg = background
-		self.bars = [self.bars[i] for i in range(len(self.bars)) if self.determine(i)]
+		self.img = self.bar_container
+		super().add_to_frame(background, self.x_offset + self.w//2, self.y)
+		super().changealpha(self.bar_container, 0.999)
 		#
 		# cv2.rectangle(blank, (self.w // 2 - 1, 0), (self.w // 2 + 1, self.h), (255, 255, 255, 255), -1, cv2.LINE_AA)
 		# self.orig_img = blank
