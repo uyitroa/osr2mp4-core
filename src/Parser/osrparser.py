39ad70e1933bf833406a70015c69522f40bed4fc
shintaroDesu <snkraishin87@gmail.com>
2020-04-30 16:30:54 +0200

    smoll update

--- src/Parser/osrparser.py
+++ src/Parser/osrparser.py
@@ -31,11 +31,18 @@ def setupReplay(osrfile, start_time, end_time):
 
 		replay_data[index] = [None, None, None, None]
 		replay_data[index][CURSOR_X] = replay_info.play_data[index].x
-		replay_data[index][CURSOR_Y] = replay_info.play_data[index].y
+		replay_data[index][CURSOR_Y] = 384 - replay_info.play_data[index].y
 		replay_data[index][KEYS_PRESSED] = replay_info.play_data[index].keys_pressed
 		replay_data[index][TIMES] = total_time
 
 	replay_data = replay_data[start_index:-1]
 	replay_data.sort(key=lambda x: x[TIMES])  # sort replay data based on time
 	start_time = replay_data[0][TIMES]
+
+	for x in range(10):
+		replay_data.append([replay_data[-1][CURSOR_X], replay_data[-1][CURSOR_Y], 0, int(replay_data[-1][TIMES]+17)])
+
+	replay_data.append([0, 0, 0, replay_data[-1][3] * 5])
+	replay_data.append([0, 0, 0, replay_data[-1][3] * 5])
+
 	return replay_data, start_time
--- src/main.py
+++ src/main.py
@@ -20,34 +20,11 @@ MOVE_DOWN = int(HEIGHT * 0.1)
 INPUTOVERLAY_STEP = 23
 
 
-def divide_core(max_osr, beatmap, skin, replay_event, resultinfo):
-	cpu_count = multiprocessing.cpu_count()
-	frame_processes = cpu_count - 1
-	processes = []
-	osr_interval = int(max_osr / frame_processes)
-
-	start_index = 0
-	outputlistfile = open("mylist.txt", "w")
-	for x in range(frame_processes):
-		if x == frame_processes-1:
-			end_index = max_osr
-		else:
-			end_index = start_index + osr_interval
-
-		filename = "process" + str(x) + ".mkv"
-		processes.append(Process(target=create_frame, args=(filename, beatmap, skin, replay_event, resultinfo, start_index, end_index)))
-		start_index += osr_interval
-
-		outputlistfile.write("file " + filename + "\n")
-	outputlistfile.close()
-	return processes
-
-
 def main():
 
 	skin_path = "../res/skin4/"
-	beatmap_file = "../res/thegame.osu"
-	replay_file = "../res/thegame.osr"
+	beatmap_file = "../res/dada.osu"
+	replay_file = "../res/dada.osr"
 
 	skin = Skin(skin_path)
 	beatmap = read_file(beatmap_file, PLAYFIELD_SCALE, skin.colours)
@@ -59,14 +36,9 @@ def main():
 	# a = open("tengaku.txt", "w")
 	# a.write(str(beatmap.hitobjects))
 	# a.close()
-	for x in range(10):
-		replay_event.append([replay_event[-1][CURSOR_X], replay_event[-1][CURSOR_Y], 0, int(replay_event[-1][TIMES]+1000/FPS)])
-
-	replay_event.append([0, 0, 0, replay_event[-1][3] * 5])
-	replay_event.append([0, 0, 0, replay_event[-1][3] * 5])
 
 	resultinfo = checkmain(beatmap, replay_event, cur_time)
-
+	print(resultinfo[-1].accuracy)
 	create_frame("output.mkv", beatmap, skin, skin_path, replay_event, resultinfo, 0, len(replay_event) - 3)
 	os.system("ffmpeg -i output.mkv -codec copy output.mp4 -y")
 
