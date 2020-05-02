b8081730bcd0dc6d5537199c3557988d77898d35
shintaroDesu <snkraishin87@gmail.com>
2020-05-02 16:41:31 +0200

    refactor frameobjects and prepareframe into another file

---
+++ src/AFrames.py
@@ -0,0 +1,86 @@
+from ImageProcess.Objects.Scores.ScoreNumbers import ScoreNumbers
+from ImageProcess.Objects.Components.Followpoints import FollowPointsManager
+from ImageProcess.Objects.Components.TimePie import TimePie
+from ImageProcess.Objects.HitObjects.CircleNumber import Number
+from ImageProcess.Objects.HitObjects.Slider import SliderManager
+from ImageProcess.Objects.HitObjects.Spinner import SpinnerManager
+from ImageProcess.Objects.Scores.Accuracy import Accuracy
+from ImageProcess.Objects.Scores.ComboCounter import ComboCounter
+from ImageProcess.Objects.Scores.Hitresult import HitResult
+from ImageProcess.Objects.Scores.ScoreCounter import ScoreCounter
+from ImageProcess.Objects.HitObjects.Circles import CircleManager
+from ImageProcess.Objects.HitObjects.Manager import HitObjectManager
+from ImageProcess.Objects.Components.Button import InputOverlay, InputOverlayBG, ScoreEntry
+from ImageProcess.Objects.Components.Cursor import Cursor, Cursortrail
+from ImageProcess.Objects.Scores.SpinBonusScore import SpinBonusScore
+from ImageProcess.Objects.Scores.URBar import URBar
+from ImageProcess.PrepareFrames.Components.Button import prepare_scoreentry, prepare_inputoverlaybg, \
+	prepare_inputoverlay
+from ImageProcess.PrepareFrames.Components.Cursor import prepare_cursor, prepare_cursortrail
+from ImageProcess.PrepareFrames.Components.Followpoints import prepare_fpmanager
+from ImageProcess.PrepareFrames.HitObjects.CircleNumber import prepare_hitcirclenumber
+from ImageProcess.PrepareFrames.HitObjects.Circles import prepare_circle, calculate_ar
+from ImageProcess.PrepareFrames.HitObjects.Slider import prepare_slider
+from ImageProcess.PrepareFrames.HitObjects.Spinner import prepare_spinner
+from ImageProcess.PrepareFrames.Scores.Accuracy import prepare_accuracy
+from ImageProcess.PrepareFrames.Scores.ComboCounter import prepare_combo
+from ImageProcess.PrepareFrames.Scores.Hitresult import prepare_hitresults
+from ImageProcess.PrepareFrames.Scores.ScoreCounter import prepare_scorecounter
+from ImageProcess.PrepareFrames.Scores.SpinBonusScore import prepare_spinbonus
+from ImageProcess.PrepareFrames.Scores.URBar import prepare_bar
+
+
+class PreparedFrames:
+	def __init__(self, path, skin, check, beatmap, settings):
+		self.cursor = prepare_cursor(path, settings.scale)
+		self.cursor_trail = prepare_cursortrail(path, settings.scale)
+		self.scoreentry = prepare_scoreentry(path, settings.scale, skin.colours["InputOverlayText"])
+		self.inputoverlayBG = prepare_inputoverlaybg(path, settings.scale)
+		self.key = prepare_inputoverlay(path, settings.scale, [255, 255, 0])
+		self.mouse = prepare_inputoverlay(path, settings.scale, [255, 0, 255])
+		self.scorenumbers = ScoreNumbers(skin.fonts, path, settings.scale)
+		self.hitcirclenumber = prepare_hitcirclenumber(path, skin.fonts, beatmap.diff, settings.playfieldscale)
+		self.accuracy = prepare_accuracy(self.scorenumbers)
+		self.combocounter = prepare_combo(self.scorenumbers)
+		self.hitresult = prepare_hitresults(path, settings.scale)
+		self.spinbonus = prepare_spinbonus(self.scorenumbers)
+		self.scorecounter = prepare_scorecounter(self.scorenumbers)
+		self.urbar = prepare_bar(settings.scale, check.scorewindow)
+		self.fpmanager = prepare_fpmanager(path, settings.playfieldscale)
+		self.circle = prepare_circle(beatmap, path, settings.playfieldscale, skin, settings.fps)
+		self.slider = prepare_slider(path, beatmap.diff, settings.playfieldscale, skin, settings.fps)
+		self.spinner = prepare_spinner(path, settings.playfieldscale)
+
+
+class FrameObjects:
+	def __init__(self, frames, skin, beatmap, check, settings):
+		opacity_interval, timepreempt, _ = calculate_ar(beatmap.diff["ApproachRate"], settings.fps)
+
+		self.cursor = Cursor(frames.cursor)
+		self.cursor_trail = Cursortrail(frames.cursor_trail)
+		# self.lifegraph = LifeGraph(skin_path + "scorebar-colour")
+
+		self.scoreentry = ScoreEntry(frames.scoreentry)
+
+		self.inputoverlayBG = InputOverlayBG(frames.inputoverlayBG)
+		self.key1 = InputOverlay(frames.key, self.scoreentry)
+		self.key2 = InputOverlay(frames.key, self.scoreentry)
+		self.mouse1 = InputOverlay(frames.mouse, self.scoreentry)
+		self.mouse2 = InputOverlay(frames.mouse, self.scoreentry)
+
+		self.accuracy = Accuracy(frames.accuracy, skin.fonts["ScoreOverlap"], settings)
+		self.timepie = TimePie(settings.scale, self.accuracy)
+		self.hitresult = HitResult(frames.hitresult, settings)
+		self.spinbonus = SpinBonusScore(frames.spinbonus, skin.fonts["ScoreOverlap"], settings)
+		self.combocounter = ComboCounter(frames.combocounter, skin.fonts["ScoreOverlap"], settings)
+		self.scorecounter = ScoreCounter(frames.scorecounter, beatmap.diff, skin.fonts["ScoreOverlap"], settings)
+
+		self.urbar = URBar(frames.urbar, settings)
+
+		self.followpoints = FollowPointsManager(frames.fpmanager, settings)
+
+		self.hitcirclenumber = Number(frames.hitcirclenumber, skin.fonts, opacity_interval)
+		self.circle = CircleManager(frames.circle, timepreempt, self.hitcirclenumber)
+		self.slider = SliderManager(frames.slider, beatmap.diff, skin, settings)
+		self.spinner = SpinnerManager(frames.spinner, settings)
+		self.hitobjmanager = HitObjectManager(self.circle, self.slider, self.spinner, check.scorewindow[2])
--- src/create_frames.py
+++ src/create_frames.py
@@ -7,37 +7,8 @@ import cv2
 from PIL import Image
 from multiprocessing import Process, Pipe
 from multiprocessing.sharedctypes import RawArray
-from ImageProcess.Objects.Components.Followpoints import FollowPointsManager
-from ImageProcess.Objects.Components.TimePie import TimePie
-from ImageProcess.Objects.HitObjects.CircleNumber import Number
-from ImageProcess.Objects.HitObjects.Slider import SliderManager
-from ImageProcess.Objects.HitObjects.Spinner import SpinnerManager
-from ImageProcess.Objects.Scores.Accuracy import Accuracy
-from ImageProcess.Objects.Scores.ComboCounter import ComboCounter
-from ImageProcess.Objects.Scores.Hitresult import HitResult
-from ImageProcess.Objects.Scores.ScoreCounter import ScoreCounter
-from ImageProcess.Objects.Scores.ScoreNumbers import ScoreNumbers
-from ImageProcess.Objects.HitObjects.Circles import CircleManager
-from ImageProcess.Objects.HitObjects.Manager import HitObjectManager
-from ImageProcess.Objects.Components.Button import InputOverlay, InputOverlayBG, ScoreEntry
-from ImageProcess.Objects.Components.Cursor import Cursor, Cursortrail
-from ImageProcess.Objects.Scores.SpinBonusScore import SpinBonusScore
-from ImageProcess.Objects.Scores.URBar import URBar
 from CheckSystem.Judgement import DiffCalculator
-from ImageProcess.PrepareFrames.Components.Button import prepare_scoreentry, prepare_inputoverlaybg, \
-	prepare_inputoverlay
-from ImageProcess.PrepareFrames.Components.Cursor import prepare_cursor, prepare_cursortrail
-from ImageProcess.PrepareFrames.Components.Followpoints import prepare_fpmanager
-from ImageProcess.PrepareFrames.HitObjects.CircleNumber import prepare_hitcirclenumber
-from ImageProcess.PrepareFrames.HitObjects.Circles import prepare_circle, calculate_ar
-from ImageProcess.PrepareFrames.HitObjects.Slider import prepare_slider
-from ImageProcess.PrepareFrames.HitObjects.Spinner import prepare_spinner
-from ImageProcess.PrepareFrames.Scores.Accuracy import prepare_accuracy
-from ImageProcess.PrepareFrames.Scores.ComboCounter import prepare_combo
-from ImageProcess.PrepareFrames.Scores.Hitresult import prepare_hitresults
-from ImageProcess.PrepareFrames.Scores.ScoreCounter import prepare_scorecounter
-from ImageProcess.PrepareFrames.Scores.SpinBonusScore import prepare_spinbonus
-from ImageProcess.PrepareFrames.Scores.URBar import prepare_bar
+from AFrames import *
 from InfoProcessor import Updater
 import numpy as np
 from skip import skip
@@ -62,62 +33,6 @@ Settings = recordclass("Settings", "width height fps scale playfieldscale movedo
 CursorEvent = recordclass("CursorEvent", "event old_x old_y")
 
 settings = Settings(WIDTH, HEIGHT, FPS, SCALE, PLAYFIELD_SCALE, MOVE_DOWN, MOVE_RIGHT)
-
-
-class PreparedFrames:
-	def __init__(self, path, skin, check, beatmap):
-		self.cursor = prepare_cursor(path, SCALE)
-		self.cursor_trail = prepare_cursortrail(path, SCALE)
-		self.scoreentry = prepare_scoreentry(path, SCALE, skin.colours["InputOverlayText"])
-		self.inputoverlayBG = prepare_inputoverlaybg(path, SCALE)
-		self.key = prepare_inputoverlay(path, SCALE, [255, 255, 0])
-		self.mouse = prepare_inputoverlay(path, SCALE, [255, 0, 255])
-		self.scorenumbers = ScoreNumbers(skin.fonts, path, SCALE)
-		self.hitcirclenumber = prepare_hitcirclenumber(path, skin.fonts, beatmap.diff, PLAYFIELD_SCALE)
-		self.accuracy = prepare_accuracy(self.scorenumbers)
-		self.combocounter = prepare_combo(self.scorenumbers)
-		self.hitresult = prepare_hitresults(path, SCALE)
-		self.spinbonus = prepare_spinbonus(self.scorenumbers)
-		self.scorecounter = prepare_scorecounter(self.scorenumbers)
-		self.urbar = prepare_bar(SCALE, check.scorewindow)
-		self.fpmanager = prepare_fpmanager(path, PLAYFIELD_SCALE)
-		self.circle = prepare_circle(beatmap, path, PLAYFIELD_SCALE, skin, FPS)
-		self.slider = prepare_slider(path, beatmap.diff, PLAYFIELD_SCALE, skin, FPS)
-		self.spinner = prepare_spinner(path, PLAYFIELD_SCALE)
-
-
-class FrameObjects:
-	def __init__(self, frames, skin, beatmap, check):
-		opacity_interval, timepreempt, _ = calculate_ar(beatmap.diff["ApproachRate"], FPS)
-
-		self.cursor = Cursor(frames.cursor)
-		self.cursor_trail = Cursortrail(frames.cursor_trail)
-		# self.lifegraph = LifeGraph(skin_path + "scorebar-colour")
-
-		self.scoreentry = ScoreEntry(frames.scoreentry)
-
-		self.inputoverlayBG = InputOverlayBG(frames.inputoverlayBG)
-		self.key1 = InputOverlay(frames.key, self.scoreentry)
-		self.key2 = InputOverlay(frames.key, self.scoreentry)
-		self.mouse1 = InputOverlay(frames.mouse, self.scoreentry)
-		self.mouse2 = InputOverlay(frames.mouse, self.scoreentry)
-
-		self.accuracy = Accuracy(frames.accuracy, skin.fonts["ScoreOverlap"], settings)
-		self.timepie = TimePie(SCALE, self.accuracy)
-		self.hitresult = HitResult(frames.hitresult, settings)
-		self.spinbonus = SpinBonusScore(frames.spinbonus, skin.fonts["ScoreOverlap"], settings)
-		self.combocounter = ComboCounter(frames.combocounter, skin.fonts["ScoreOverlap"], settings)
-		self.scorecounter = ScoreCounter(frames.scorecounter, beatmap.diff, skin.fonts["ScoreOverlap"], settings)
-
-		self.urbar = URBar(frames.urbar, settings)
-
-		self.followpoints = FollowPointsManager(frames.fpmanager, settings)
-
-		self.hitcirclenumber = Number(frames.hitcirclenumber, skin.fonts, opacity_interval)
-		self.circle = CircleManager(frames.circle, timepreempt, self.hitcirclenumber)
-		self.slider = SliderManager(frames.slider, beatmap.diff, skin, settings)
-		self.spinner = SpinnerManager(frames.spinner, settings)
-		self.hitobjmanager = HitObjectManager(self.circle, self.slider, self.spinner, check.scorewindow[2])
 
 
 def nearer(cur_time, replay, index):
@@ -275,7 +190,7 @@ def setup_draw(beatmap, frames, replay_event, resultinfo, shared, skin, start_in
 
 	time_preempt = diffcalculator.ar()
 
-	component = FrameObjects(frames, skin, beatmap, diffcalculator)
+	component = FrameObjects(frames, skin, beatmap, diffcalculator, settings)
 
 	component.cursor_trail.set_cursor(old_cursor_x, old_cursor_y)
 
@@ -386,7 +301,7 @@ def write_frame(shared, conn, filename, codec):
 def create_frame(filename, codec, beatmap, skin, skin_path, replay_event, resultinfo, start_index, end_index, mpp):
 
 	diffcalculator = DiffCalculator(beatmap.diff)
-	frames = PreparedFrames(skin_path, skin, diffcalculator, beatmap)
+	frames = PreparedFrames(skin_path, skin, diffcalculator, beatmap, settings)
 
 	if mpp >= 1:
 		shared_array = []
