3e3df8674290a0e6f873b0ec5ae1230b9daaf039
shintaroDesu <snkraishin87@gmail.com>
2020-05-04 05:05:20 +0200

    smooth follow fadeout

--- src/ImageProcess/Objects/HitObjects/Manager.py
+++ src/ImageProcess/Objects/HitObjects/Manager.py
@@ -58,13 +58,13 @@ class HitObjectManager:
 		self.circle_manager.circles[timestamp][9] = 1
 
 	def sliderchangestate(self, followappear, timestamp):
-		index_interval = 0.65 * self.rate
+		index_interval = 1 # * self.rate
 
 		if self.slider_manager.sliders[timestamp].sliderb_i != self.slider_manager.slidermax_index:
 			self.slider_manager.sliders[timestamp].sliderb_i = 0
 
 		if followappear:
-			index_interval = -0.65 * self.rate
+			index_interval = -0.65 # * self.rate
 			self.slider_manager.sliders[timestamp].sliderb_i = self.slider_manager.slidermax_index - 3
 
 		self.slider_manager.sliders[timestamp].appear_f = index_interval
