ad08735106883c91745be91dee406b9ccbf7acaf
shintaroDesu <snkraishin87@gmail.com>
2020-05-08 00:49:43 +0200

    scorebar origin topleft

--- src/ImageProcess/Objects/FrameObject.py
+++ src/ImageProcess/Objects/FrameObject.py
@@ -17,5 +17,5 @@ class FrameObject:
 	def h(self):
 		return self.frames[self.frame_index].size[1]
 
-	def add_to_frame(self, background, x_offset, y_offset, alpha=1):
-		imageproc.add(self.frames[int(self.frame_index)], background, x_offset, y_offset, alpha=alpha)
+	def add_to_frame(self, background, x_offset, y_offset, alpha=1, topleft=False):
+		imageproc.add(self.frames[int(self.frame_index)], background, x_offset, y_offset, alpha=alpha, topleft=topleft)
