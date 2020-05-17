80e47fd8a056e94d0c98e70d96c0d310b1dd57ef
shintaroDesu <snkraishin87@gmail.com>
2020-05-18 00:32:22 +0200

    update health

--- src/CheckSystem/Health.py
+++ src/CheckSystem/Health.py
@@ -32,7 +32,7 @@ class HealthDummy:
 
 class HealthProcessor:
 	def __init__(self, beatmap, drainrate=None):
-		self.multiplier = {0: -1, 50: -0.05, 105: 0.2, 15: 0.35, 35: 0.5, 100: 0.5, 300: 1}
+		self.multiplier = {0: -1, 50: -0.05, 100: 0.5, 300: 1}
 		self.minimum_health_error = 0.01
 		self.min_health_target = 0.95
 		self.mid_health_target = 0.70
@@ -90,7 +90,7 @@ class HealthProcessor:
 				currentHealth = min(1, currentHealth + step)
 
 				if "slider" in self.beatmap[i]["type"]:
-					step = len(self.beatmap[i]["ticks pos"]) * self.health_increase_for(15)
+					step = len(self.beatmap[i]["ticks pos"]) * self.health_increase_for(100)
 					currentHealth = min(1, currentHealth + step)
 
 				if lowestHealth < 0:
