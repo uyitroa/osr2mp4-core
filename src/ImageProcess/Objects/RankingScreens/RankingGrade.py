5426d052ee14b3bddf7bff6c0e07a435ae75a041
shintaroDesu <snkraishin87@gmail.com>
2020-05-24 16:41:42 +0200

    a lot of 50 wont give S grade

--- src/ImageProcess/Objects/RankingScreens/RankingGrade.py
+++ src/ImageProcess/Objects/RankingScreens/RankingGrade.py
@@ -15,7 +15,12 @@ class RankingGrade(ARankingScreen):
 
 		total = replayinfo.number_300s + replayinfo.number_100s + replayinfo.number_50s + replayinfo.misses
 		p300 = replayinfo.number_300s/total
-		self.playergrade = max(6, int(p300 * 10)) - int(replayinfo.misses > 0)
+		self.playergrade = max(5, int(p300 * 10)) - int(replayinfo.misses > 0)
+		self.playergrade = max(5, self.playergrade)
+
+		# Over 90% 300s, less than 1% 50s and no misses
+		p50 = replayinfo.number_50s/total
+		self.playergrade -= int(self.playergrade == 9 and p50 >= 0.01)
 
 		self.gradeframe = gradeframes[self.grades[self.playergrade]]
 		self.gap = int(gap * Settings.scale * 0.75)
