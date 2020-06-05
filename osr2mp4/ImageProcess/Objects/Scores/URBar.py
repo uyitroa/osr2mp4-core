19325664ea1b309e1a25b9b6fc89930103f11ef3
shintaroDesu <snkraishin87@gmail.com>
2020-06-05 14:22:39 +0200

    fix urbar size

--- osr2mp4/ImageProcess/Objects/Scores/URBar.py
+++ osr2mp4/ImageProcess/Objects/Scores/URBar.py
@@ -15,6 +15,8 @@ class URBar(FrameObject):
 		self.bars = []
 		self.resultdict = {50: 0, 100: 1, 300: 2}
 
+		self.c = 0
+
 		self.bar_container = Image.new("RGBA", (self.w, self.h))
 		self.urbar, self.bar_images, self.maxtime = frames
 
@@ -22,17 +24,17 @@ class URBar(FrameObject):
 		pos = int(self.w/2 + delta_t/self.maxtime * self.w/2)
 		# self.bars.append([pos, 1, self.resultdict[hitresult]])
 		img = self.bar_images[self.resultdict[hitresult]]
-		imageproc.add(img, self.bar_container, pos, self.bar_container.size[1]//2)
+		imageproc.add(img, self.bar_container, pos, self.bar_container.size[1]//2, channel=4)
 		s = self.bar_container.size
 		self.bar_container.paste((255, 255, 255, 255), (s[0]//2 - 1, 0, s[0]//2 + 1, s[1]))
 
 	def add_to_frame_bar(self, background):
 		img = self.urbar
 		imageproc.add(img, background, self.x, self.y)
-		img = Image.new("RGBA", (4, self.h), (255, 255, 255, 255))
-		imageproc.add(img, background, self.x, self.y)
 
 	def add_to_frame(self, background):
+		self.c += 1
 		img = self.bar_container
 		imageproc.add(img, background, self.x_offset + self.w//2, self.y)
-		imageproc.addalpha(self.bar_container, -0.001)
+		if self.c % 4 == 0:
+			imageproc.addalpha(self.bar_container, -0.00001)
