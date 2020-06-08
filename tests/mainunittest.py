ae51deb0bcc6ce346b3fceabb5b37f898639fc01
shintaroDesu <snkraishin87@gmail.com>
2020-06-08 18:18:43 +0200

    update tests

--- tests/mainunittest.py
+++ tests/mainunittest.py
@@ -2,11 +2,11 @@ from audionametest import *
 from diffmultiplier_test import *
 from modcombinations_test import *
 from moddiff_test import *
-from framestest import *
+# from framestest import *
 from scoretest import *
 from sliderfollowtest import *
 from rankingscreentest import *
-from specificframetest import *
+# from specificframetest import *
 from spinnerpositiontest import *
 from rankingscreencomparetest import *
 from specificframecomparetest import *
