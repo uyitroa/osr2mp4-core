1d8398f8274ea619d5efa0097b62218a4bb67057
shintaroDesu <snkraishin87@gmail.com>
2020-06-29 16:36:19 +0200

    fix special char by removing replacing \ by / line

--- osr2mp4/Parser/jsonparser.py
+++ osr2mp4/Parser/jsonparser.py
@@ -4,7 +4,7 @@ import json
 def read(filename):
 	fileopen = open(filename, "r", encoding="utf-8")
 	text = fileopen.read()
-	text = text.replace("\\", "/")
+	# text = text.replace("\\", "/")
 	data = json.loads(text)
 
 	fileopen.close()
