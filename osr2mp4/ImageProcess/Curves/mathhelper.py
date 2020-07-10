e189abb08a42b5ad01fd45ab3b8027cdbcec6731
shintaroDesu <snkraishin87@gmail.com>
2020-07-11 00:59:47 +0200

    add catmull, still work in progress

--- osr2mp4/ImageProcess/Curves/mathhelper.py
+++ osr2mp4/ImageProcess/Curves/mathhelper.py
@@ -28,6 +28,12 @@ def catmull(p, t): # WARNING:   Worst math formula incomming
         (2 * p[0] - 5 * p[1] + 4 * p[2] - p[3]) * pow(t, 2) +
         (-p[0] + 3 * p[1] - 3 * p[2] + p[3]) * pow(t, 3))
 
+def cdistance(p, p1):
+    x = p[0] - p1[0]
+    y = p[1] - p1[1]
+    return pow(x * x + y * y, 0.5)
+
+
 def point_on_line(p0, p1, length):
     full_length = pow(pow(p1.x - p0.x, 2) + pow(p1.y - p0.y, 2), 0.5)
     n = full_length - length
@@ -37,13 +43,13 @@ def point_on_line(p0, p1, length):
     return Vec2(x, y)
 
 def angle_from_points(p0, p1):
-    return math.atan2(p1.y - p0.y, p1.x - p0.x)
+    return math.atan2(p1[1] - p0[1], p1[0] - p0[0])
 
 def distance_from_points(array):
     distance = 0
 
     for i in range(1, len(array)):
-        distance += array[i].distance(array[i - 1])
+        distance += cdistance(array[i], array[i - 1])
 
     return distance
 
@@ -51,7 +57,7 @@ def cart_from_pol(r, t):
     x = (r * math.cos(t))
     y = (r * math.sin(t))
 
-    return Vec2(x, y)
+    return [x, y]
 
 def point_at_distance(array, distance): #TODO: Optimize...
     i = 0
@@ -59,7 +65,7 @@ def point_at_distance(array, distance): #TODO: Optimize...
     new_distance = 0
 
     if len(array) < 2:
-        return Vec2(0, 0)
+        return [0, 0]
 
     if distance == 0:
         return array[0]
@@ -68,8 +74,8 @@ def point_at_distance(array, distance): #TODO: Optimize...
         return array[len(array) - 1]
 
     for i in range(len(array) - 2):
-        x = (array[i].x - array[i + 1].x)
-        y = (array[i].y - array[i + 1].y)
+        x = (array[i][0] - array[i + 1][0])
+        y = (array[i][1] - array[i + 1][1])
 
         new_distance = math.sqrt(x * x + y * y)
         current_distance += new_distance
@@ -85,10 +91,10 @@ def point_at_distance(array, distance): #TODO: Optimize...
         angle = angle_from_points(array[i], array[i + 1])
         cart = cart_from_pol((distance - current_distance), angle)
 
-        if array[i].x > array[i + 1].x:
-            coord = Vec2((array[i].x - cart.x), (array[i].y - cart.y))
+        if array[i][0] > array[i + 1][0]:
+            coord = [(array[i][0] - cart[0]), (array[i][1] - cart[1])]
         else:
-            coord = Vec2((array[i].x + cart.y), (array[i].y + cart.y))
+            coord = [(array[i][1] + cart[1]), (array[i][1] + cart[1])]
 
         return coord
 
