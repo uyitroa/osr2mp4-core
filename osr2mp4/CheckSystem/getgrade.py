0af4ae07cc00169bafa73e7580c16210611f0f7a
shintaroDesu <snkraishin87@gmail.com>
2020-07-15 19:33:33 +0200

    fix grade

--- osr2mp4/CheckSystem/getgrade.py
+++ osr2mp4/CheckSystem/getgrade.py
@@ -2,14 +2,18 @@ from ..EEnum.EGrade import Grade
 
 
 def getgrade(acc):
-	grades = {10: Grade.SS, 9: Grade.S, 8: Grade.A, 7: Grade.B, 6: Grade.C, 5: Grade.D}
-
 	total = acc[300] + acc[100] + acc[50] + acc[0]
 	p300 = acc[300] / total
-	playergrade = max(5, int(p300 * 10)) - int(acc[0] > 0)
-	playergrade = max(5, playergrade)
-
-	# Over 90% 300s, less than 1% 50s and no misses
 	p50 = acc[50] / total
-	playergrade -= int(playergrade == 9 and p50 >= 0.01)
-	return grades[playergrade]
+	if p300 == 1.0:
+		return Grade.SS
+	elif p300 > 0.9 and p50 < 0.01 and acc[0] == 0:
+		return Grade.S
+	elif (p300 > 0.9 and acc[0] > 0) or (p300 > 0.8 and acc[0] == 0):
+		return Grade.A
+	elif (p300 > 0.7 and acc[0] == 0) or (p300 > 0.8 and acc[0] > 0):
+		return Grade.B
+	elif p300 > 0.6:
+		return Grade.C
+
+	return Grade.D
