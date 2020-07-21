4ff03f4c1e58eee8d34b923acea6b24c7c91852b
shintaroDesu <snkraishin87@gmail.com>
2020-07-21 16:50:10 +0200

    move default var to global_var.py

--- osr2mp4/global_var.py
+++ osr2mp4/global_var.py
@@ -1,4 +1,73 @@
+import os
+
 from .osrparse.enums import Mod
+
+
+defaultsettings = {
+	"Cursor size": 1,
+	"In-game interface": True,
+	"Show scoreboard": True,
+	"Background dim": 100,
+	"Rotate sliderball": False,
+	"Always show key overlay": True,
+	"Automatic cursor size": False,
+	"Show mods icon": True,
+	"Enable PP counter": False,
+	"Score meter size": 1,
+	"Song volume": 50,
+	"Effect volume": 50,
+	"Ignore beatmap hitsounds": False,
+	"Use skin's sound samples": False,
+	"Song delay": 0,
+	"Global leaderboard": False,
+	"Mods leaderboard": "*",
+	"api key": "lol",
+}
+
+defaultppconfig = {
+	"x": 1320,
+	"y": 725,
+	"Size": 25,
+	"Rgb": [
+		255,
+		255,
+		255
+	],
+	"Alpha": 1,
+	"Font": "arial.ttf",
+	"Background": os.path.join(os.path.dirname(__file__), "res/pptemplate.png"),
+	"Hitresult x": 50,
+	"Hitresult y": 150,
+	"Hitresult Size": 16,
+	"Hitresult Rgb": [
+		255,
+		255,
+		255
+	],
+	"Hitresult Alpha": 1,
+	"Hitresult Font": "arial.ttf",
+	"Hitresult Background": os.path.join(os.path.dirname(__file__), "res/hitresulttemplate.png"),
+	"Hitresult Gap": 3
+}
+
+
+# source: ggjjwp
+sortedmods = [
+	Mod.NoFail,
+	Mod.Easy,
+	Mod.Hidden,
+	Mod.HardRock,
+	Mod.Relax,
+	Mod.SuddenDeath,
+	Mod.HalfTime,
+	Mod.Nightcore,
+	Mod.DoubleTime,
+	Mod.Flashlight,
+	Mod.Perfect,
+	Mod.Autopilot,
+	Mod.SpunOut,
+	Mod.Autoplay
+]
 
 
 class Settings:
@@ -29,69 +98,10 @@ class Settings:
 		self.moveright = None
 		self.timeframe = None
 
-		self.settings = {
-			"Cursor size": 1,
-			"In-game interface": True,
-			"Show scoreboard": True,
-			"Background dim": 100,
-			"Rotate sliderball": False,
-			"Always show key overlay": True,
-			"Automatic cursor size": False,
-			"Score meter size": 1,
-			"Song volume": 50,
-			"Effect volume": 50,
-			"Ignore beatmap hitsounds": False,
-			"Use skin's sound samples": False,
-			"Global leaderboard": False,
-			"Mods leaderboard": "*",
-			"api key": "lol"
-		}
+		self.settings = defaultsettings
 
-		self.ppsettings = {
-			"x": 1240,
-			"y": 725,
-			"Size": 25,
-			"Rgb": [
-				255,
-				255,
-				255
-			],
-			"Alpha": 1,
-			"Font": "arial.ttf",
-			"Background": "",
-			"Hitresult x": 50,
-			"Hitresult y": 150,
-			"Hitresult Size": 16,
-			"Hitresult Rgb": [
-				255,
-				255,
-				255
-			],
-			"Hitresult Alpha": 1,
-			"Hitresult Font": "arial.ttf",
-			"Hitresult Background": "",
-			"Hitresult Gap": 3
-		}
+		self.ppsettings = defaultppconfig
 
 		self.codec = None
 		self.process = None
 		self.enablelog = False
-
-
-# source: ggjjwp
-sortedmods = [
-	Mod.NoFail,
-	Mod.Easy,
-	Mod.Hidden,
-	Mod.HardRock,
-	Mod.Relax,
-	Mod.SuddenDeath,
-	Mod.HalfTime,
-	Mod.Nightcore,
-	Mod.DoubleTime,
-	Mod.Flashlight,
-	Mod.Perfect,
-	Mod.Autopilot,
-	Mod.SpunOut,
-	Mod.Autoplay
-]
