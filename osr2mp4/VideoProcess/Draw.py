67bb1da516e3ad63b118c50570fee3c62f4fe991
shintaroDesu <snkraishin87@gmail.com>
2020-07-26 12:58:26 +0200

    fix draw order of hitresutl

--- osr2mp4/VideoProcess/Draw.py
+++ osr2mp4/VideoProcess/Draw.py
@@ -86,9 +86,9 @@ class Drawer:
 		cursor_y = int(self.cursor_event.event[Replays.CURSOR_Y] * self.settings.playfieldscale) + self.settings.movedown
 
 		self.component.background.add_to_frame(self.img, self.np_img, self.frame_info.cur_time, in_break)
-		self.component.hitresult.add_to_frame(self.img)
 		self.component.followpoints.add_to_frame(self.img, self.frame_info.cur_time)
 		self.component.hitobjmanager.add_to_frame(self.img, self.frame_info.cur_time)
+		self.component.hitresult.add_to_frame(self.img)
 		self.component.flashlight.add_to_frame(self.img, in_break, cursor_x, cursor_y)
 
 		self.component.scorebarbg.add_to_frame(self.img, self.frame_info.cur_time, in_break)
