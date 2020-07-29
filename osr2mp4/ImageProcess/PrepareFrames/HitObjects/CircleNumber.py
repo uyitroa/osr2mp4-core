335fb7fa8307882df0ad22e4a3665b6fcec88d90
shintaroDesu <snkraishin87@gmail.com>
2020-07-29 23:39:40 +0200

    fix grade fl

--- osr2mp4/ImageProcess/PrepareFrames/HitObjects/CircleNumber.py
+++ osr2mp4/ImageProcess/PrepareFrames/HitObjects/CircleNumber.py
@@ -13,6 +13,7 @@ def prepare_hitcirclenumber(diff, scale, settings):
 	circle_radius = (54.4 - 4.48 * diff["CircleSize"]) * scale * 0.8
 	hitcircle_number = []
 	scale = circle_radius * 2 / default_circle_size
+	scale *= 1.05
 	for x in range(10):
 		img = YImage("-{}".format(str(x)), settings, scale, prefix="HitCirclePrefix")
 		hitcircle_number.append(img.img)
