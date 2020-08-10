69f4442f390fdc84394105059dcc0ccb72455831
shintaroDesu <snkraishin87@gmail.com>
2020-08-10 20:55:57 +0200

    fixed #59

--- osr2mp4/Parser/osuparser.py
+++ osr2mp4/Parser/osuparser.py
@@ -112,11 +112,11 @@ class Beatmap:
 		timing = timing.split("\n")
 		inherited = 0
 		for line in timing:
+			my_dict = {}
 			try:
 				line = line.strip()
 				if line == '':
 					continue
-				my_dict = {}
 				items = line.split(",")
 				my_dict["Offset"] = float(items[0])
 				if len(items) < 7 or int(items[6]) == 1:
@@ -130,10 +130,16 @@ class Beatmap:
 				my_dict["SampleIndex"] = items[4]
 				my_dict["Volume"] = float(items[5])
 			except Exception as e:
+				my_dict["Meter"] = my_dict.get("Meter", 0)
+				my_dict["SampleSet"] = my_dict.get("SampleSet", "0")
+				my_dict["SampleIndex"] = my_dict.get("SampleIndex", "0")
+				my_dict["Volume"] = my_dict.get("Volume", 100)
+				self.timing_point.append(my_dict)
 				logging.error(repr(e))
 				continue
 			# my_dict["Kiai"] = int(items[7])
 			self.timing_point.append(my_dict)
+		self.timing_point.append({"Offset": float('inf')})
 		self.timing_point.append({"Offset": float('inf')})
 
 	def istacked(self, curobj, prevobj, t_min, end=""):
@@ -204,7 +210,6 @@ class Beatmap:
 			my_dict["time"] = int(osuobject[2])
 			my_dict["id"] = index
 
-			# use next off_set or not
 			while my_dict["time"] >= self.timing_point[cur_offset + 1]["Offset"]:
 				cur_offset += 1
 			my_dict["BeatDuration"] = self.timing_point[cur_offset]["BeatDuration"]
