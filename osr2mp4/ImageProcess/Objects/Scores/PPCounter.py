0344adaacdc5b3ab57a6d5135f964154b7ec1111
shintaroDesu <snkraishin87@gmail.com>
2020-08-14 16:59:04 +0200

    smooth pp counter

--- osr2mp4/ImageProcess/Objects/Scores/PPCounter.py
+++ osr2mp4/ImageProcess/Objects/Scores/PPCounter.py
@@ -5,9 +5,33 @@ class PPCounter(ACounter):
 	def __init__(self, settings):
 		super().__init__(settings, settings.ppsettings)
 		self.score = str(0.00)
+		self.realscore = 0
+		self.timeframe = self.settings.timeframe / self.settings.fps
+		self.first = True
 
 	def update(self, pp):
-		self.set(pp)
+		self.realscore = pp
+		if self.first:
+			self.set(pp)
+			self.first = False
 
 	def set(self, pp):
 		self.score = "{:.2f}".format(pp)
+
+	def easingout(self, time, initial, change, duration):
+		t = time / duration
+		return -change * t * (t - 2) + initial
+
+	def add_to_frame(self, background):
+
+		score = float(self.score)
+
+		super().add_to_frame(background)
+
+		current = self.timeframe
+		change = self.realscore - score
+		duration = 500
+
+		score = max(0, self.easingout(current, score, change, duration))
+
+		self.score = "{:.2f}".format(score)
