0fb8b8c7b536ba47cde328c0e1710a5f321e76ac
shintaroDesu <snkraishin87@gmail.com>
2020-08-27 11:29:41 +0200

    ok

--- osr2mp4/ImageProcess/Curves/libcurves/setup.py
+++ osr2mp4/ImageProcess/Curves/libcurves/setup.py
@@ -5,7 +5,7 @@ from Cython.Distutils import build_ext
 ext_modules = [Extension(
     name="ccurves",
     sources=["ccurves.pyx", 'BezierApproximator.cpp', 'Vector2.cpp', 'PerfectApproximator.cpp', 'LinearApproximator.cpp', 'CatmullApproximator.cpp', 'curves.cpp'],
-    extra_compile_args=["-std=c++11"],
+    extra_compile_args=["-std=c++11", "-w"],
     language="c++",
     )]
 
--- osr2mp4/VideoProcess/FFmpegWriter/setup.py
+++ osr2mp4/VideoProcess/FFmpegWriter/setup.py
@@ -6,7 +6,7 @@ import numpy
 
 setup(name="zzz", cmdclass = {'build_ext': build_ext},
 	ext_modules=[
-		Extension("osr2mp4cv", sources=["PyFrameWriter.pyx", "FrameWriter.cpp"], extra_compile_args=["-std=c++11"],
+		Extension("osr2mp4cv", sources=["PyFrameWriter.pyx", "FrameWriter.cpp"], extra_compile_args=["-std=c++11", "-w"],
                 language="c++",
                 include_dirs = ['include', numpy.get_include()],
         libraries = ['avcodec', 'avutil', 'avformat', 'swresample', 'swscale'],
