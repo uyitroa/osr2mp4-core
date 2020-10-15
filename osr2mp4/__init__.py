596915e4d827fd1a8ab6187610d87a46a1986b0e
Chris de Graaf <me@cdg.dev>
2020-10-14 20:32:25 -0500

    Try to redirect ffmpeg output to log stream

--- osr2mp4/__init__.py
+++ osr2mp4/__init__.py
@@ -1,6 +1,17 @@
 #import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 import logging
+import os
+import sys
 
 logger = logging.getLogger(__name__)
 logger.propagate = False
+
+
+def log_stream():
+	if not logger.handlers:
+		return sys.stdout
+	handler = logger.handlers[0]
+	if hasattr(handler, "stream"):
+		return handler.stream
+	return open(os.devnull, "w")
