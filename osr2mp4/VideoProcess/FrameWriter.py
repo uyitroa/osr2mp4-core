29fe07619cf84fb2eb25a4ef21fb92e843452b8f
Your Name <you@example.com>
2020-11-20 00:15:48 -0800

    optimal threads

--- osr2mp4/VideoProcess/FrameWriter.py
+++ osr2mp4/VideoProcess/FrameWriter.py
@@ -35,7 +35,7 @@ def getwriter(filename, settings, buf):
 		ffmpegargs = settings.settings["FFmpeg custom commands"]
 		writer = FFMpegWriter(settings.ffmpeg, settings.output, (settings.width, settings.height), settings.fps, ffmpegcodec, 
 			settings.temp + 'audio.mp3', settings.audiocodec, "ultrafast", str(settings.settings["Audio bitrate"]) + "k", 
-			logfile = log_stream(), threads = 4,  pixel_format = "bgr24")
+			logfile = log_stream(), threads = 0,  pixel_format = "bgr24")
 
 	return writer
 
