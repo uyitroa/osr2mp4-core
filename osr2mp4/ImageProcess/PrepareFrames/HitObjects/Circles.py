38f7b5fb7cf374d6fcd3eaf5ac4346f23568e068
shintaroDesu <snkraishin87@gmail.com>
2020-12-22 17:51:21 +0100

    idk

--- osr2mp4/ImageProcess/PrepareFrames/HitObjects/Circles.py
+++ osr2mp4/ImageProcess/PrepareFrames/HitObjects/Circles.py
@@ -77,8 +77,8 @@ def overlayapproach(circle, approach, alpha):
 
 def prepare_fadeout(img, settings):
 	timescale = 60/settings.fps * settings.timeframe/1000
-	fade_out = alpha.fadeout(img, 1, 0, 0.07 * timescale)
-	fade_out = size.grow(fade_out, 1.1, 1.7, 0.05 * timescale)
+	fade_out = alpha.fadeout(img, 1, 0, 0.05 * timescale)
+	fade_out = size.grow(fade_out, 1.01, 1.15, 0.03 * timescale)
 	return fade_out
 
 
