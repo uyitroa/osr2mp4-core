861fe1ff413adc34c2937742e4520c0ffb61445e
uyitroa <shintaridesu@gmail.com>
2020-12-22 11:03:04 +0100

    Update Draw.py

--- osr2mp4/VideoProcess/Draw.py
+++ osr2mp4/VideoProcess/Draw.py
@@ -163,7 +163,7 @@ class Drawer:
 
 	def draw_rankingpanel(self):
 		if not self.initialised_ranking:
-			#self.settings.timeframe = 1000
+			self.settings.timeframe = 1000
 			self.initialiseranking()
 			self.initialised_ranking = True
 		
