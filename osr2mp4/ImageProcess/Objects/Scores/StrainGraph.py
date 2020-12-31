d6602a850ae8b528628a45209e440730cf854f7e
Your Name <you@example.com>
2020-12-31 11:55:26 -0800

    better timing

--- osr2mp4/ImageProcess/Objects/Scores/StrainGraph.py
+++ osr2mp4/ImageProcess/Objects/Scores/StrainGraph.py
@@ -4,7 +4,7 @@ from osr2mp4.ImageProcess.PrepareFrames.YImage import YImage
 from PIL import Image
 
 class StrainGraph(FrameObject):
-	def __init__(self, settings, beatmap_start_time, beatmap_end_time):
+	def __init__(self, settings, start_time, end_time):
 		self.settings = settings
 		if(settings.settings["Enable Strain Graph"]):
 			self.scale = settings.scale * settings.strainsettings["Size"]/20
@@ -13,10 +13,16 @@ class StrainGraph(FrameObject):
 			self.alpha = settings.strainsettings["Alpha"]
 			self.graph = None
 			self.graph_pixel_access = None
-			self.start_time = beatmap_start_time
-			self.end_time = beatmap_end_time
 			self.last_position = 0
 			self.width, self.height = (0,0)
+			self.start_time = start_time
+			self.end_time = end_time
+			self.beatmap = None
+			self.object_index = 0
+
+	def set_beatmap(self, beatmap):
+		self.beatmap = beatmap
+		print(self.beatmap)
 
 	def set_strain_graph(self, filename):
 		if(self.settings.settings["Enable Strain Graph"]):
@@ -26,8 +32,17 @@ class StrainGraph(FrameObject):
 			self.width, self.height = self.graph.size
 			return True
 
+	def get_closest_object_time(self, cur_time):
+		if self.object_index >= len(self.beatmap) or self.beatmap[self.object_index].time >= cur_time:
+			return cur_time
+		while self.object_index < len(self.beatmap) and self.beatmap[self.object_index].time < cur_time:
+			result = self.beatmap[self.object_index].time
+			self.object_index += 1
+			return result
+
 	def update_progress(self, cur_time):
-		ratio = (cur_time-self.start_time)/max(self.end_time-self.start_time, 1)
+		time = self.get_closest_object_time(cur_time)
+		ratio = (time-self.start_time)/max(self.end_time-self.start_time, 1)
 		progress = min(self.width, int(ratio*self.graph.size[0]))
 		if( (progress >= 0) & (progress > self.last_position) ):
 			self.set_graph_progress_opacity(self.last_position, progress, self.settings.strainsettings["ProgressAlpha"])
